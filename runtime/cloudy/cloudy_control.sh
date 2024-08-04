2e7a6668cb32e10cd591a9c9ec00355e7133e20a
Derek Coffman <derek.coffman@noaa.gov>
2024-08-04 23:35:22 +0000

    update startup scripts

--- runtime/cloudy/cloudy_control.sh
+++ runtime/cloudy/cloudy_control.sh
@@ -22,3 +22,43 @@ if [ $target = "sensors" ]; then
     echo python bin/envds.py "$run_command" "$target" -id cloudy -f $(pwd)/runtime/cloudy/"$target"_"$power_bus".txt
     python bin/envds.py $run_command $target -id cloudy -f $(pwd)/runtime/cloudy/"$target"_"$power_bus".txt
 fi
+
+if [ $target = "all" ]; then
+    echo python bin/envds.py power 12v-2 -s "$power_state"
+    python bin/envds.py power 12v-2 -s $power_state
+
+    echo python bin/envds.py power 12v-1 -s "$power_state"
+    python bin/envds.py power 12v-1 -s $power_state
+
+    echo python bin/envds.py power 28v -s "$power_state"
+    python bin/envds.py power pitot_tube_enable -s off
+    python bin/envds.py power cdp_enable -s off
+    python bin/envds.py power 28v -s $power_state
+
+    # sleep 5s
+
+    # services
+    echo "$run_command" services...
+    python bin/envds.py $run_command services -id cloudy -f $(pwd)/runtime/cloudy/services.txt
+
+    echo "$run_command" interfaces...
+    python bin/envds.py $run_command interfaces -id cloudy -f $(pwd)/runtime/cloudy/interfaces.txt
+
+    if [ $run_command = "startup" ]; then
+        echo waiting for interfaces to start...
+        sleep 10
+    fi
+
+    echo "$run_command" sensors...
+    python bin/envds.py $run_command sensors -id cloudy -f $(pwd)/runtime/cloudy/sensors.txt
+
+    if [ $run_command = "startup" ]; then
+        sleep 10
+        echo enable cdp...
+        python bin/envds.py power cdp_enable -s on
+    fi
+
+    # do mSEMS specific restart?
+
+    echo done.
+fi
